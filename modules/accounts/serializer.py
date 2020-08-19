7ddb63c1c3814e2955cab9e62a06728748a804c5
Maicol <maicolcrodrigoa@gmail.com>
2020-08-18 23:42:54 -0500

    fixed accounts serializer

--- modules/accounts/serializer.py
+++ modules/accounts/serializer.py
@@ -5,11 +5,9 @@ class AccountsPostSerializer(Schema):
     class Meta:
         strict = True
 
-    id = fields.Integer()
     user = fields.String()
     name = fields.String(
         required=True, validate=validate.Length(min=4, max=50))
-    friendCollectionUrl = fields.String()
 
 
 class AccountPutSerializer(AccountsPostSerializer):
